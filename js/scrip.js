630ce78ea11540ed148ab4e6dfdd85c8986aa96a
Mariam Cabaleiro Arán <mariamcabaleiro@gmail.com>
2024-11-12 10:06:24 -0300

    jsnumero

--- js/scrip.js
+++ js/scrip.js
@@ -1,37 +1,24 @@
-/*let num1;
-console.log("Numero uno:",num1)
-let num2;
-console.log("Numero dos:",num2)
-let suma = (num1+num2)
-
-console.log("Resultado de la suma:",suma)
-*/
-
-//PIDE EL MENSAJE PERO SOLO UNO
-/*let numero1 = prompt("Ingrese un numero1: ");
-numero = parseInt(numero)
+let numero1 = prompt("Ingrese un numero1: ");
+numero1 = parseInt(numero1)
 console.log("Numero1:", numero1);
 
 let numero2 = prompt("Ingrese un numero2: ");
-numero = parseInt(numero)
+numero2 = parseInt(numero2)
+console.log("Numero2:", numero2);
 
 suma = numero1 + numero2;
 console.log("Suma:", suma);
-*/
-//PIDE EL MENSAJE PERO SOLO UNO
+
+
 let nombre = prompt("Ingrese su nombre: ");
 document.write("<p>Hola " + nombre + "</p>");
 
 let edad = prompt("Ingrese su edad: ");
-edad = parseInt(numero)
-//NO FUNCIONA
+edad = parseInt(edad)
+
 if (edad >=18) {
     console.log("Sos mayor de edad");
   }
   else {
     console.log("Sos menor de edad");
   }
-//PIDE EL MENSAJE PERO SOLO UNO
-
-//PEDIR CODIGO CLASE10
-
